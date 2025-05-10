459ca5a482ab5b5f9a58e731b44d480d4f780c1e
Kamil Cały <kamil.caly@vp.pl>
2025-05-10 15:04:35 +0200

    logged user can add given products to cart

--- Shop_MVC/wwwroot/js/products.js
+++ Shop_MVC/wwwroot/js/products.js
@@ -1,4 +1,6 @@
-﻿function collectFilter() {
+﻿const shop_mvc_basket = 'shop_mvc_basket';
+
+function collectFilter() {
     const category = $('input[name="categoryRadio"]:checked').val();
     const company = $('input[name="companyRadio"]:checked').val();
     const color = $('input[name="colorRadio"]:checked').val();
@@ -31,6 +33,7 @@ function updateProducts() {
         data: JSON.stringify(filter),
         success: function (html) {
             $('#products-container').html(html);
+            updateProductViewBasedOnUserBasket();
         },
         error: function (xhr, status, error) {
             console.error('Error during filtering:', error);
@@ -38,6 +41,69 @@ function updateProducts() {
         }
     });
 }
+
+// product: Shop_MVC.Entities.Product, amount: +1 or -1
+function addToCart(product, amount) {
+    //debugger;
+    const basket = localStorage.getItem(shop_mvc_basket);
+    if (!basket) {
+        if (amount === -1) return;
+        const firstProduct = {
+            Id: product.Id,
+            Quantity: amount
+        }
+        const products = [firstProduct];
+        localStorage.setItem(shop_mvc_basket, JSON.stringify(products));
+    } else {
+        let basketProducts = JSON.parse(basket);
+        const existingProduct = basketProducts.find(p => p.Id === product.Id);
+        if (amount === -1 && (existingProduct?.Quantity === 1)) {
+            basketProducts = basketProducts.filter(p => p.Id !== existingProduct.Id);
+            showAddToCartBtnOnly(existingProduct.Id);
+        }
+        else if (existingProduct) {
+            existingProduct.Quantity += amount;
+        } else {
+            basketProducts.push({
+                Id: product.Id,
+                Quantity: amount
+            });
+        }
+        localStorage.setItem(shop_mvc_basket, JSON.stringify(basketProducts));
+    }
+
+    updateProductViewBasedOnUserBasket();
+}
+
+
+function updateProductViewBasedOnUserBasket() {
+    const basket = localStorage.getItem(shop_mvc_basket);
+    if (!basket) return;
+
+    const basketProducts = JSON.parse(basket);
+    basketProducts.forEach(product => {
+        //debugger;
+        let display_btn = "none";
+        let display_div = "flex";
+        if (product.Quantity == 0) {
+            display_btn = "block";
+            display_div = "none";
+        }
+        if ($('#user_identity_name_span').length === 0) {
+            display_btn = "block";
+            display_div = "none";
+        }
+        $(`#add_to_cart_btn_${product.Id}`).css("display", display_btn);
+        $(`#add_to_cart_div_${product.Id}`).css("display", display_div);
+        $(`#add_to_cart_div_${product.Id}`).children('div').text(product.Quantity);
+    });
+}
+
+function showAddToCartBtnOnly(productId) {
+    $(`#add_to_cart_btn_${productId}`).css("display", "block");
+    $(`#add_to_cart_div_${productId}`).css("display", "none");
+}
+
 
 $(function () {
     $('input[type=radio]').on('change', function () {
@@ -53,4 +119,6 @@ $(function () {
         $('#sortDropdownBtn').text(selectedText);
         updateProducts();
     });
+
+    updateProductViewBasedOnUserBasket();
 });
